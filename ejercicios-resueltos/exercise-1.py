1e5289b0fa5217e273bf71a0ee3babc0877c2433
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2024-01-05 11:53:48 -0400

    update files

--- ejercicios-resueltos/exercise-1.py
+++ ejercicios-resueltos/exercise-1.py
@@ -1,3 +1,12 @@
+'''
+1. Create an online banking system with the following features:
+    * Users must be able to log in with a username and password.
+    * If the user enters the wrong credentials three times, the system must lock them out.
+    * The initial balance in the bank account is $2000.
+    * The system must allow users to deposit, withdraw, view, and transfer money.
+    * The system must display a menu for users to perform transactions.
+'''
+
 # Funcion para acceso al sistema
 def login(): 
     # Se definen las credenciales iniciales
