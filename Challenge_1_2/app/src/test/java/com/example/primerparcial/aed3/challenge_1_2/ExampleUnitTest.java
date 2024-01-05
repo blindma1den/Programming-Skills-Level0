81752374fe7aa60579c6448cae5dcd368362f493
ladyrobotina <valerisvso@gmail.com>
2024-01-05 01:10:34 -0300

    Challenge 1

---
+++ Challenge_1_2/app/src/test/java/com/example/primerparcial/aed3/challenge_1_2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.primerparcial.aed3.challenge_1_2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
