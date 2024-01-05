b467ec8efa8ba3cf34e7577da31b892cc1fac691
Jonathan Ismael Santamaría Lunar <jonathanisl@gmail.com>
2024-01-05 15:37:01 -0400

    [FEAT] University Enrollment System

---
+++ Finance_Management_Application/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Management_Application;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Online_Shipping_System/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Shipping_System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
