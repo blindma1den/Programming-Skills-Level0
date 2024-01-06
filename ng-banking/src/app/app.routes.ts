5a94a9a13bbd630fd1874762488d9c24519a728e
pattopilco <pattopilco@gmail.com>
2024-01-05 23:51:19 -0500

    user login with 3 times

---
+++ ng-banking/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import  { LoginComponent} from './pages/login/login.component'
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
---
+++ ng-banking/src/app/models/user.ts
@@ -0,0 +1,6 @@
+export interface UserDTO{
+  username:string;
+  password:string;
+  times:number;
+}
+
