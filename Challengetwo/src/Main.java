a5cf5ec8bc1a14037e3dfce2b8decf68de89d734
Fernando Cardozo <cardozof123@outlook.com>
2024-01-07 17:24:12 -0500

    FINALLY

--- Challengetwo/src/Main.java
+++ Challengetwo/src/Main.java
@@ -1,20 +1,12 @@
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // int fixedValue = 2000;
         bankMenu();
-
-
-        /*
-        switch (scanner.next()){
-            case "USD" -> {
-
-            }
-        }
-         */
     }
 
 
@@ -33,7 +25,7 @@ public class Main {
                 System.out.println("Currently unavailable");
                 break;
             case 5:
-                converterMenu();
+                converterMenu(moneyAsker());
                 break;
             case 6:
                 System.out.println("Cya");
@@ -43,7 +35,6 @@ public class Main {
                 break;
         }
     }
-
     static int mainMenu() {
         Scanner scanner = new Scanner(System.in);
 
@@ -61,8 +52,7 @@ public class Main {
 
         return numberOption;
     }
-
-    static String converterMenu() {
+    static void converterMenu(double amount) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Orange Currency Converter ");
@@ -78,11 +68,12 @@ public class Main {
         String option = scanner.next();
         System.out.println("The option choosen was: " + option);
 
-        return specificMenu(option);
+        double usd = convertToUSD(option, amount);
+        specificMenu(option, usd);
     }
-
-    static String specificMenu(String optionChosen) {
+    static void specificMenu(String optionChosen, double usd) {
         Scanner scanner = new Scanner(System.in);
+        String optionChosen2 = null;
         switch (optionChosen) {
             case "USD", "EUR", "ARS", "CLP", "TRY", "GBP" -> {
                 System.out.println("Choose the currency you want to convert to: ");
@@ -93,7 +84,7 @@ public class Main {
                 System.out.println("-> TRY");
                 System.out.println("-> GBP");
                 System.out.print("Type the Initial Currency you want to use (USD, EUR, ...): ");
-                String optionChosen2 = scanner.next();
+                optionChosen2 = scanner.next();
                 System.out.println("The option chosen was: " + optionChosen2);
 
                 while (optionChosen2.equals(optionChosen)){
@@ -110,11 +101,30 @@ public class Main {
                 }
             }
         }
-        return optionChosen;
+        optionChosen = optionChosen2;
+        subMenu(convert(usd,optionChosen), optionChosen);
     }
 
+    static void subMenu(double finalAmount, String optionChosen){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("The amount of money after the conversion is: " + df.format(finalAmount) + optionChosen);
+        System.out.println("Select one of these following options: ");
+        System.out.println("1. Withdraw");
+        System.out.println("2. Back to Main Menu");
+        int option = scanner.nextInt();
 
-    static Double convertToUSD(String key, int fixedValue){
+        while (option != 1 && option != 2){
+            System.out.print("Wrong Option, try again: ");
+            option = scanner.nextInt();
+        }
+
+        if (option == 1){
+            withdraw(finalAmount, optionChosen);
+        } else {
+            bankMenu();
+        }
+    }
+    static Double convertToUSD(String key, double amount){
         HashMap<String, Double> initialCurrency = new HashMap<>();
 
         initialCurrency.put("USD", 1.00);
@@ -122,12 +132,11 @@ public class Main {
         initialCurrency.put("ARS", 0.0012);
         initialCurrency.put("GBP", 1.27);
         initialCurrency.put("TRY", 0.034);
-        initialCurrency.put("CLP", 0.0011);
+        initialCurrency.put("CLP", 0.00112);
 
         // ESTO DEVUELVE DOLARES
-        return fixedValue/initialCurrency.get(key);
+        return (amount * initialCurrency.get(key));
     }
-
     static Double convert(double usd, String monedaFinal){
         HashMap<String, Double> usdConverter = new HashMap<String, Double>();
 
@@ -138,7 +147,59 @@ public class Main {
         usdConverter.put("TRY", 29.81);
         usdConverter.put("GBP", 0.79);
 
-        return usd * usdConverter.get(monedaFinal);
+        return (usd * usdConverter.get(monedaFinal));
+    }
+
+    static void withdraw(double finalAmount, String optionChosen) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How much money do you want to withdraw?");
+        double cantidad = scanner.nextDouble();
+        double verif= cantidad + (cantidad*0.01);
+        if ( verif > finalAmount && cantidad < 0) {
+            System.out.println("Invalid quantity, please try again");
+            withdraw(finalAmount, optionChosen);
+        } else {
+            finalAmount = finalAmount - verif;
+            System.out.println("Total amount: " + df.format(finalAmount) + optionChosen);
+            menuAnotherOperation();
+        }
+    }
+
+    static int moneyAsker(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Minimum and Maximum amount per currency");
+        System.out.println("USD -> [Min: 500, Max: 100000]");
+        System.out.println("EUR -> [Min: 500, Max: 100000]");
+        System.out.println("CLP -> [Min: 500, Max: 100000]");
+        System.out.println("ARS -> [Min: 500, Max: 100000]");
+        System.out.println("TRY -> [Min: 500, Max: 100000]");
+        System.out.println("GBP -> [Min: 500, Max: 100000]");
+        System.out.print("Please, insert how much money you want to convert: ");
+        int money = scanner.nextInt();
+        if ((money >= 500) && (money <= 100000)){
+            return money;
+        }else{
+            System.out.println("Error, invalid amount. Try again.");
+            moneyAsker();
+        }
+        return money;
+    }
+
+    static void menuAnotherOperation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to perform another operation?");
+        System.out.println("1. Yes");
+        System.out.println("2. No, leave me alone");
+        int option = scanner.nextInt();
+
+        while (option != 1 && option != 2) {
+            System.out.print("Wrong Option, try again: ");
+            option = scanner.nextInt();
+        }
+
+        if (option == 1) {
+            bankMenu();
+        }
     }
 }
 
