61897b5937712b752298e841a1e19c56f149fae3
Daniel Limón <dlimon.munoz@gmail.com>
2024-01-08 01:31:37 -0600

    University Enrollment Implemented!

--- dlimon2_solutions/main.py
+++ dlimon2_solutions/main.py
@@ -11,11 +11,13 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY')
 from _code.blueprints.banking_system import bankingsystem
 from _code.blueprints.shipping_system import shippingsystem
 from _code.blueprints.currency_exchange import currencyexchange
+from _code.blueprints.university_enrollment import universityenrollment
 
 # register blueprints
 app.register_blueprint(bankingsystem)
 app.register_blueprint(shippingsystem)
 app.register_blueprint(currencyexchange)
+app.register_blueprint(universityenrollment)
 
 
 @app.route('/')
