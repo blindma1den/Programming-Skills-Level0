fb19fec09900ca90b5b3d5426b697819a3101c55
Andrea Otálora <andreaotalora9509@gmail.com>
2024-01-08 18:14:38 -0500

    file .py

---
+++ converter.py
@@ -0,0 +1,6 @@
+# 2. Create a currency converter between CLP, ARS, USD, EUR, TRY, GBP with the following features:
+# * 		The user must choose their initial currency and the currency they want to exchange to.
+# * 		The user can choose whether or not to withdraw their funds. If they choose not to withdraw, it should return to the main menu.
+# * 		If the user decides to withdraw the funds, the system will charge a 1% commission.
+# * 		Set a minimum and maximum amount for each currency, it can be of your choice.
+# * 		The system should ask the user if they want to perform another operation. If they choose to do so, it should restart the process; otherwise, the system should close.
