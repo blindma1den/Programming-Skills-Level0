4e4f85cb1af949d982386f1736cbf397234129e5
jorgecordoba1 <jorgecordoba@me.com>
2024-01-08 15:42:25 -0500

    50%

--- 002currencyConverter/script.js
+++ 002currencyConverter/script.js
@@ -5,3 +5,95 @@
 * 		Set a minimum and maximum amount for each currency, it can be of your choice.
 * 		The system should ask the user if they want to perform another operation. If they choose to do so, it should restart the process; otherwise, the system should close.*/
 
+let usdToClp = 903.83;
+let usdToArs = 813.91;
+let usdToEur = 0.91;
+let usdToTry = 29.90;
+let usdToGbp = 0.78;
+let usdToUsd = 1;
+
+/*let clpFirst = document.getElementById("clpFirst");
+let arsFirst = document.getElementById("arsFirst");
+let usdFirst = document.getElementById("usdFirst");
+let eurFirst = document.getElementById("eurFirst");
+let tryFirst = document.getElementById("tryFirst");
+let gbpFirst = document.getElementById("gbpFirst");*/
+
+let clpSelected = document.getElementById("clpSelected");
+let arsSelected = document.getElementById("arsSelected");
+let usdSelected = document.getElementById("usdSelected");
+let eurSelected = document.getElementById("eurSelected");
+let trySelected = document.getElementById("trySelected");
+let gbpSelected = document.getElementById("gbpSelected");
+
+clpSelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToClp;
+    document.getElementById("total").innerText = result;
+});
+
+arsSelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToArs;
+    document.getElementById("total").innerText = result;
+});
+
+usdSelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToUsd;
+    document.getElementById("total").innerText = result;
+});
+
+eurSelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToEur;
+    document.getElementById("total").innerText = result;
+});
+
+trySelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToTry;
+    document.getElementById("total").innerText = result;
+});
+
+gbpSelected.addEventListener("click", function() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let firstCoinRateValue = firstCoinRate(); 
+    let dollars = amount * firstCoinRateValue;
+    let result = dollars * usdToGbp;
+    document.getElementById("total").innerText = result;
+});
+
+function firstCoinRate() {
+    let firstCoin = document.querySelector('input[name="firstCoin"]:checked').id;
+    let rate;
+
+    if (firstCoin === "clpFirst") {
+        rate = 0.0011;
+    } else if (firstCoin === "arsFirst") {
+        rate = 0.0012;
+    } else if (firstCoin === "eurFirst") {
+        rate = 1.10;
+    } else if (firstCoin === "tryFirst") {
+        rate = 0.033;
+    } else if (firstCoin === "gbpFirst") {
+        rate = 1.27;
+    } else if (firstCoin === "usdFirst") {
+        rate = 1;
+    }
+
+    return rate;
+}
+
+
+
