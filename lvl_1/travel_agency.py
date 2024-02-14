3a1db34963fe8e83a005c4e91be671fc1bac33bc
Silvestre Arismendi <sa.arismendij@gmail.com>
2024-02-14 01:26:47 -0400

    Asking activites to do in a trip added

--- lvl_1/travel_agency.py
+++ lvl_1/travel_agency.py
@@ -13,7 +13,20 @@ Design a system that helps users choose their best destination according to thei
 Clue: You could consider the user's budget.
 """
 
-activities = ['skiing', 'tour', 'hiking', 'extreme sports', 'activities',]
+user_info = {
+  'budget': 0,
+  'interests': [],
+  }
+
+activities = [
+  'skiing',
+  'tour',
+  'hiking',
+  'extreme sports',
+  'beach activities',
+  'cultural and historical tour',
+  'cultural and historical activities'
+  ]
 
 countries = {
   'Andorra': 'skiing',
@@ -29,16 +42,106 @@ countries = {
 spain_list = countries['Spain']
 belgium_list = countries['Belgium']
 
-def test():
-  traveler_preferences = input('Enter: ')
-  if traveler_preferences in countries.values():
-    print('NICE!')
-  elif traveler_preferences in spain_list or traveler_preferences in belgium_list:
-    print('NICE! x2')
+"""
+- Ask budget (DONE!)
+- Ask preferences/interests (DONE!)
+- Show what it is affordable to the user
+"""
 
+def budget_requirement():
+  min_budget = 100
+  ask_budget = int(input('How much budget do you have?\n=> $'))
+
+  if ask_budget < min_budget:
+    print('You do not have enough money to take a trip 😢')
+  else:
+    print(f'Your budget is: ${ask_budget}')
+
+def ask_activities():
+  i = 0
+
+  print('Activities that you can do in your trip:')
+  for activity in activities:
+    i += 1
+    print(f'{i}. {activity.capitalize()}')
+
+  if len(user_info['interests']) > 0:
+    print('\nThese are the activities that you are interested:')
+    for interesting_activity in user_info['interests']:
+      print(f'- {interesting_activity.capitalize()}')
+
+  try:
+    traveler_preferences = int(input('\nEnter the number: '))
+    match traveler_preferences:
+      case 1:
+        if activities[0] in user_info['interests']:
+          print(f'\n** You already chose "{activities[0].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[0])
+          print(f'\n"{activities[0].capitalize()}" added to your interests.\n')
+      case 2:
+        if activities[1] in user_info['interests']:
+          print(f'\n** You already chose "{activities[1].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[1])
+          print(f'\n"{activities[1].capitalize()}" added to your interests.\n')
+      case 3:
+        if activities[2] in user_info['interests']:
+          print(f'\n** You already chose "{activities[2].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[2])
+          print(f'\n"{activities[2].capitalize()}" added to your interests.\n')
+      case 4:
+        if activities[3] in user_info['interests']:
+          print(f'\n** You already chose "{activities[3].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[3])
+          print(f'\n"{activities[3].capitalize()}" added to your interests.\n')
+      case 5:
+        if activities[4] in user_info['interests']:
+          print(f'\n** You already chose "{activities[4].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[4])
+          print(f'\n"{activities[4].capitalize()}" added to your interests.\n')
+      case 6:
+        if activities[5] in user_info['interests']:
+          print(f'\n** You already chose "{activities[5].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[5])
+          print(f'\n"{activities[5].capitalize()}" added to your interests.\n')
+      case 7:
+        if activities[6] in user_info['interests']:
+          print(f'\n** You already chose "{activities[6].capitalize()}". Try again. **\n')
+          ask_activities()
+        else:
+          user_info['interests'].append(activities[6])
+          print(f'\n"{activities[6].capitalize()}" added to your interests.\n')
+      case _:
+        print('\n** That activity does not exist. **\n')
+        ask_activities()
+  except ValueError:
+    print('\n** Type numbers. Not letters. **\n')
+    ask_activities()
+
+  while True:
+    try_again = input('Do you want to add another interest? Y/N: ')
+    try_again = try_again.upper().strip()
+    if try_again == 'Y':
+      print()
+      ask_activities()
+    elif try_again == 'N':
+      exit()
+    else:
+      print('That option does not exist.')
 
 def run_app():
-  print('Running!')
-  test()
+  # budget_requirement()
+  ask_activities()
 
 run_app()
